fd27cbc10cb75f6cf9f037eac1ec42d5af175cc6
Sabrinna <sabrinnastslt@gmail.com>
2025-04-21 19:29:38 -0300

    Adicionado musica

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 function showLove() {
     document.getElementById("result").classList.remove("hidden");
     document.getElementById("sadResult").classList.add("hidden");
+
+    stopSadSound();
+    playLoveSound();
 }
 
 function showSad() {
     document.getElementById("sadResult").classList.remove("hidden");
     document.getElementById("result").classList.add("hidden");
+
+    stopLoveSound();
+    playSadSound();
+}
+
+function playLoveSound() {
+    const loveAudio = document.getElementById("loveAudio");
+    loveAudio.currentTime = 0;
+    loveAudio.play();
+}
+
+function stopLoveSound() {
+    const loveAudio = document.getElementById("loveAudio");
+    loveAudio.pause();
+    loveAudio.currentTime = 0;
+}
+
+function playSadSound() {
+    const sadAudio = document.getElementById("sadAudio");
+    sadAudio.currentTime = 0;
+    sadAudio.play();
+}
+
+function stopSadSound() {
+    const sadAudio = document.getElementById("sadAudio");
+    sadAudio.pause();
+    sadAudio.currentTime = 0;
 }
 
 function moveButton() {
